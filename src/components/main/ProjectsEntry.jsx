379ae902219b2d647f21a5b64745b6816063fbc1
Aidan Saull <saull.aidan@gmail.com>
2023-10-31 03:42:06 -0400

    Added git icon and link

--- src/components/main/ProjectsEntry.jsx
+++ src/components/main/ProjectsEntry.jsx
@@ -1,4 +1,5 @@
 import image from '../images/me.jpg'
+import git from '../images/github-mark.svg'
 import { ProjectsProvider } from '../../context/ProjectsContext'
 import ProjectsList from '../main/ProjectsList'
 
@@ -16,6 +17,9 @@ const ProjectsEntry = () => {
                         <p className='blurb'> Some stuff I've done over the last few years that I think is interesting/relevant to whomever has received this link.</p>
                         <h3 className='year'>2019-2023</h3>
                         <h2 className='subtitle'>Version: 1.0</h2>
+                        <a href="https://github.com/ASaull">
+                            <img src={git} className="githubIcon" />
+                        </a>
                     </div>
                     <div className = "column right">
                         <div className='blankspace'/>
