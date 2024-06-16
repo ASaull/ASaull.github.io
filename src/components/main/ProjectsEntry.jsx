654e802c9bc4d60c98d03f53877abb48ab6fd397
Aidan Saull <saull.aidan@gmail.com>
2024-06-16 03:55:04 -0400

    Updated style to neumorphism

--- src/components/main/ProjectsEntry.jsx
+++ src/components/main/ProjectsEntry.jsx
@@ -1,5 +1,5 @@
 import image from '../images/me.jpg'
-import git from '../images/github-mark.svg'
+import git from '../images/github-mark-white.svg'
 import { ProjectsProvider } from '../../context/ProjectsContext'
 import ProjectsList from '../main/ProjectsList'
 
@@ -11,12 +11,14 @@ const ProjectsEntry = () => {
             <div className = "container">
                 <div className = "row">
                     <div className = "column left">
-                        <img src={image} className='image'/>
+                        <div className = "aboutme">
+                            <img src={image} alt="A Picture of Aidan" className='image'/>
+                        </div>
                         <h1 className='title'> Aidan Saull </h1>
                         <h2 className='subtitle'> Selected Projects </h2>
                         <p className='blurb'> Some stuff I've done over the last few years that I think is interesting/relevant to whomever has received this link.</p>
                         <h3 className='year'>2019-2024</h3>
-                        <h2 className='subtitle'>Version: 1.0</h2>
+                        <h2 className='subtitle'>Version: 1.1 — "Neumorphic"</h2>
                         <a href="https://github.com/ASaull">
                             <img src={git} className="githubIcon" />
                         </a>
--- src/components/reusable/ProjectButton.jsx
+++ src/components/reusable/ProjectButton.jsx
@@ -12,7 +12,7 @@ function ProjectButton({ title, description, techs, id}) {
 
     if (contentRef.current) console.log(contentRef.current.scrollHeight);
 
-    return <div className="project-div" style={open ? { backgroundColor: "#f1e6e4", filter: "drop-shadow(15px 15px 0px black)" } : {}}>
+    return <div className="project-div" style={open ? { backgroundColor: "rgba(255, 255, 255, 0.5)", boxShadow: "0px 20px 20px rgba(0, 0, 0, 0.4)" } : {}}>
             <div className="row">
             <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@100" rel="stylesheet" />
             <div className="column">
@@ -25,7 +25,7 @@ function ProjectButton({ title, description, techs, id}) {
                 </ul>
             </div>
             <div className="column button-column">
-                <button className="project-button" onClick={toggle} type="button" style={open ? {opacity: "100%"} : {}}>
+                <button className="project-button" onClick={toggle} type="button" style={open ? {opacity: "100%", boxShadow: "0px 0px 0px #00000020, -0px -0px 0px #ffffff76, inset 5px 5px 10px #00000020, inset -5px -5px 10px #ffffff76"} : {}}>
                     <span className="material-symbols-outlined size-48" style={open ? {transform: "rotate(180deg)"} : {transform: "rotate(0)"} }>arrow_downward</span>
                 </button>
             </div>
